101da8844b2ab4d4b38bd88207b464ef132d7f13
chanmi <cksal4278@gmail.com>
2021-10-11 22:40:32 +0900

    등록/수정/조회 api 만들기

---
+++ src/test/java/com/chanmi/book/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,73 @@
+package com.chanmi.book.springboot.web.domain.posts;
+
+import com.chanmi.book.springboot.domain.posts.Posts;
+import com.chanmi.book.springboot.domain.posts.PostsRepository;
+import org.apache.tomcat.jni.Local;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void save_post(){
+
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+        .title(title)
+        .content(content)
+        .author("cksal4278@gmail.com")
+        .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void save_BaseTimeEntity(){
+        //given
+        LocalDateTime now = LocalDateTime.of(2021,10,11,0,0,0);
+        postsRepository.save(Posts.builder()
+        .title("title")
+        .content("content")
+        .author("author")
+        .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>>>> createDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
+}
