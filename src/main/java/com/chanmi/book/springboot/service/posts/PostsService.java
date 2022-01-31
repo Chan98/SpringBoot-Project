925a94ec6072b775b77bdb11309f29dd59ebe04c
chanmi <cksal4278@gmail.com>
2022-02-01 00:26:11 +0900

    게시글 등록, 수정, 삭제 생성(mustache)

--- src/main/java/com/chanmi/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/chanmi/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.chanmi.book.springboot.service.posts;
 
 import com.chanmi.book.springboot.domain.posts.Posts;
 import com.chanmi.book.springboot.domain.posts.PostsRepository;
+import com.chanmi.book.springboot.web.dto.PostsListResponseDto;
 import com.chanmi.book.springboot.web.dto.PostsResponseDto;
 import com.chanmi.book.springboot.web.dto.PostsSaveRequestDto;
 import com.chanmi.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,5 +36,23 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    //트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도가 개선 => 등록/수정/삭제 기능이 전혀 없는 서비스 메소드에서 사용
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                //람다식 >> map(posts -> new PostsListResponseDto(posts))
+                //postsRepository 결과로 넘어온 Posts의 Stream을 map을 통해 PostsListResponseDto 변환 -> List로 반환
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id= " + id));
+
+        postsRepository.delete(posts);
     }
 }
--- src/main/java/com/chanmi/book/springboot/web/PostsApiController.java
+++ src/main/java/com/chanmi/book/springboot/web/PostsApiController.java
@@ -13,19 +13,27 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
+    //게시글을 등록
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
     }
-
+    //게시글을 수정
     @PutMapping("api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
         return postsService.update(id, requestDto);
     }
-
+    //하나의 게시글 읽기
     @GetMapping("api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id){
         return postsService.findById(id);
+    }
+    //삭제
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+
+        return id;
     }
 
 }
