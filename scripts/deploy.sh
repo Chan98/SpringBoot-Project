8920450f3293e6551bb1ddd79a4370636618b580
chan98 <cksal4278@gmail.com>
2022-05-29 14:43:02 +0900

    프로젝트명 다시 수정 및 jar 파일 찾지 못해 8080 겹치므로 java로 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션pid 확인"
-CURRENT_PID=$(pgrep -fl springboot-project-webservice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl SpringBoot-Project | grep java | awk '{print $1}')
 
 echo "현재 구동 중인 애플리케이션pid: $CURRENT_PID"
 
